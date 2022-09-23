fc9134e451f0d554951841e00face0c6a9ff6b22
Bo-Chun Wang <bo-chun.wang@couchbase.com>
2022-09-22 21:28:53 -0700

    fix invalid non-printable character

--- ch3driver/pytpcc/util/results.py
+++ ch3driver/pytpcc/util/results.py
@@ -265,12 +265,12 @@ class Results:
             tmp = []
             tmp.append(self.simple_query_times)
             self.simple_query_times = tmp
-            tmp = []
+            tmp = []
             tmp.append(self.adv_query_times)
-            self.adv_query_times = tmp
-            tmp = []
+            self.adv_query_times = tmp
+            tmp = []
             tmp.append(self.na_query_times)
-            self.na_query_times = tmp
+            self.na_query_times = tmp
 
         fts_stats = {}
         for qry_times in self.simple_query_times: # each array element corresponds to one client
